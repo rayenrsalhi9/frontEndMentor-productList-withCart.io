64809904158fe95dbfd43c9e107c992c494473ad
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-08-18 13:19:15 +0100

    Data fetched from json and displayed to html with success

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,14 @@ async function fetchData() {
             console.log(typeof data[0].image.desktop);
 
             for (let i in data) {
-
+                /* 
                 let image = document.createElement('img');
                 image.src = data[i].image.desktop;
                 image.alt = 'product image';
 
                 document.body.appendChild(image);
+                 */
+                createProduct(data, i);
             }
         }
 
@@ -29,3 +31,60 @@ async function fetchData() {
 }
 
 fetchData();
+
+/* Selecting items */
+let productsArea = document.querySelector('.products');
+
+function createProduct(data, x) {
+
+    let product = document.createElement('div');
+    product.className = 'product';
+
+    let imageContainer = document.createElement('div');
+    imageContainer.className = 'image';
+
+    let image = document.createElement('img');
+    image.src = data[x].image.desktop;
+    image.alt = 'product image';
+    image.className = 'product-image';
+    imageContainer.appendChild(image);
+
+    let addToCartButton = document.createElement('div');
+    addToCartButton.className = 'add-to-cart';
+
+    let addToCartImage = document.createElement('img');
+    addToCartImage.src = '../../assets/images/icon-add-to-cart.svg';
+    addToCartImage.alt = 'add to cart icon';
+    addToCartButton.appendChild(addToCartImage);
+
+    let addToCartText = document.createElement('p');
+    addToCartText.innerText = 'Add To Cart';
+    addToCartButton.appendChild(addToCartText);
+
+    imageContainer.appendChild(addToCartButton);
+
+    product.appendChild(imageContainer);
+
+    let description = document.createElement('div');
+    description.className = 'description';
+
+    let category = document.createElement('p');
+    category.className = 'product-category';
+    category.innerText = data[x].category;
+
+    let name = document.createElement('p');
+    name.className = 'product-name';
+    name.innerText = data[x].name;
+
+    let price = document.createElement('p');
+    price.className = 'product-price';
+    price.innerText = data[x].price;
+
+    description.appendChild(category);
+    description.appendChild(name);
+    description.appendChild(price);
+
+    product.appendChild(description);
+
+    productsArea.appendChild(product);
+}
